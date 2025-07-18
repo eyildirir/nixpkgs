d5a74d09c6befcbd258dc04e50f1a1f44bc27122
Eren Yildirir <eren.yildirir@gmail.com>
2025-07-18 16:55:43 -0400

    Update binary-hashes.nix
    Added 2.7.0 binary sources. Hashes to be updated...

--- pkgs/development/libraries/science/math/libtorch/binary-hashes.nix
+++ pkgs/development/libraries/science/math/libtorch/binary-hashes.nix
@@ -17,4 +17,21 @@ builtins.getAttr version {
       hash = "sha256-UaX47GAwyZ6UmzgY85TeAHmy3u52pBHhiyM5NAz7ens=";
     };
   };
+  "2.7.0" = {
+    aarch64-darwin-cpu = {
+      name = "libtorch-macos-arm64-2.7.0.zip";
+      url = "https://download.pytorch.org/libtorch/cpu/libtorch-macos-arm64-2.7.0.zip";
+      hash = "sha256-4d9YKUuvAESBeG/WCUsQfEHwdB2z34grnlwWslj4970=";
+    };
+    x86_64-linux-cpu = {
+      name = "libtorch-cxx11-abi-shared-with-deps-2.7.0-cpu.zip";
+      url = "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-2.7.0%2Bcpu.zip";
+      hash = "sha256-gUzPhc4Z8rTPhIm89pPoLP0Ww17ono+/xgMW46E/Tro=";
+    };
+    x86_64-linux-cuda = {
+      name = "libtorch-cxx11-abi-shared-with-deps-2.7.0-cu128.zip";
+      url = "https://download.pytorch.org/libtorch/cu128/libtorch-cxx11-abi-shared-with-deps-2.5.0%2Bcu128.zip";
+      hash = "sha256-UaX47GAwyZ6UmzgY85TeAHmy3u52pBHhiyM5NAz7ens=";
+    };
+  };
 }
